fed692cd679cbf9b08d3448d04176c650f61f042
Chandan Tiwari <tiwarichandan29702@gmail.com>
2026-09-19 22:53:16 +0530

    Initial portfolio

---
+++ script.js
@@ -0,0 +1,98 @@
+const loginBtn = document.querySelector("#loginBtn");
+const loginModal = document.querySelector("#loginModal");
+const closeLogin = document.querySelector("#closeLogin");
+const loginForm = document.querySelector("#loginForm");
+
+loginBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    if (loginBtn.textContent === "Logout") {
+        loginBtn.textContent = "Login";
+    alert("Logout");
+    }   
+    else{
+    loginModal.style.display = "flex";
+    }
+});
+
+closeLogin.addEventListener("click", function () {
+    loginModal.style.display = "none";
+});
+
+loginForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const username =
+        document.querySelector("#username").value;
+
+    const password =
+        document.querySelector("#password").value;
+
+
+    if (username === "chandan" && password === "12345") {
+
+        alert("Login Successful!");
+
+        loginModal.style.display = "none";
+        loginBtn.textContent = "Logout";
+
+    } else {
+
+        alert("Wrong Username or Password!");
+
+    }
+
+});
+const contactForm = document.querySelector("#contactForm");
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.querySelector("#contactName").value;
+    const email = document.querySelector("#contactEmail").value;
+    const message = document.querySelector("#contactMessage").value;
+
+    alert(
+        "Message Sent Successfully!\n\n" +
+        "Name: " + name +
+        "\nEmail: " + email +
+        "\nMessage: " + message
+    );
+
+    contactForm.reset();
+}); 
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", function () {
+
+    sections.forEach(function (section) {
+
+        const sectionTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (sectionTop < windowHeight - 100) {
+            section.classList.add("show");
+        }
+
+    });
+
+});
+const topBtn = document.querySelector("#topBtn");
+
+window.addEventListener("scroll", function () {
+
+    if (window.scrollY > 400) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
